6030a65655501b50017495552a5b2a68b041b8d6
Lucas Vallejo <97030519+lucasvallejodev@users.noreply.github.com>
2024-03-31 22:30:43 +0200

    feat: basic diagram as code

---
+++ basic_architecture.py
@@ -0,0 +1,25 @@
+from diagrams import Cluster, Diagram
+from diagrams.aws.general import User
+from diagrams.custom import Custom
+from diagrams.programming.language import Javascript
+
+with Diagram("", show=False):
+    with Cluster("User"):
+      user = User("User")
+      web_browser = Custom("Web Browser", "./res/browser.png")
+
+    with Cluster("Front-end"):
+      web_app = Javascript("Web App")
+
+    with Cluster("Back-end"):
+      server = Custom("Server", "./res/server.png")
+      db = Custom("Database", "./res/database.png")
+
+
+    # connections
+    user >> web_browser
+    web_browser >> web_app
+    web_app >> server
+
+    server >> db
+    db >> server
---
+++ ci_cd_development.py
@@ -0,0 +1,25 @@
+from diagrams import Cluster, Diagram
+from diagrams.aws.general import SDK
+from diagrams.onprem.vcs import Git
+from diagrams.custom import Custom
+from diagrams.programming.language import Javascript
+
+with Diagram("", show=False):
+    with Cluster("Back-end"):
+      server = Custom("Server", "./res/server.png")
+
+    with Cluster("Development"):
+        dev = Custom("Developer", "./res/dev.png")
+        with Cluster("CI/CD"):  
+          git = Git("Repository")
+          rack = Custom("Automated Build", "./res/build.png")
+          binaries = SDK("Binaries")
+          deploy = Custom("Automated Deploy", "./res/deploy.png")
+          test = Custom("Automated Test", "./res/test.png")
+
+    dev >> git
+    git >> rack
+    rack >> binaries
+    binaries >> deploy
+    deploy >> test
+    deploy >> server
